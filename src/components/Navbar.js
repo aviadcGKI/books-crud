c61ea7413b783d520723cd9270a440bbd6eddb43
aviadc <coaviadco@gmail.com>
2022-02-28 14:54:18 +0200

    start building the navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,8 +1,30 @@
 import React from 'react'
+import { NavbarContainer, LeftNavbarContainer, RightNavbarContainer
+,NavbarInnerContainer, NavbarExtendedContainer, NavbarLinkContainer
+,NavbarLink, LogoContainer } from './styledComponents/Navbar.styled'
+import logo from '../images/logo.jpg'
 
 function Navbar() {
   return (
-    <div>Navbar</div>
+      <>
+        <NavbarContainer>
+            <NavbarInnerContainer>
+                <LeftNavbarContainer>
+                    <NavbarLinkContainer>
+                        <NavbarLink to='/'>Home</NavbarLink>
+                        <NavbarLink to='/author'>Author</NavbarLink>
+                        <NavbarLink to='/books'>Books</NavbarLink>
+                    </NavbarLinkContainer>
+                </LeftNavbarContainer>
+                <RightNavbarContainer>
+                    <LogoContainer src={logo}></LogoContainer>
+                </RightNavbarContainer>
+            </NavbarInnerContainer>
+            <NavbarExtendedContainer>
+
+            </NavbarExtendedContainer>
+        </NavbarContainer>
+      </>
   )
 }
 
--- src/components/styledComponents/Navbar.styled.js
+++ src/components/styledComponents/Navbar.styled.js
@@ -1,5 +1,59 @@
 import styled from "styled-components";
+import { Link } from "react-router-dom";
 
-const NavbarContainer = styled.nav`
-    
+export const NavbarContainer = styled.nav`
+    width: 100%;
+    height: 9vh;
+    background-color: black;
+    color: white;
+    display: flex;
+    flex-direction: column;
+    /* gap: 1rem; */
+`;
+
+export const LeftNavbarContainer = styled.div`
+    display: flex;
+    flex: 70%;
+    align-items: center;
+`;
+
+
+export const RightNavbarContainer = styled.div`
+    display: flex;
+    flex: 30%;
+    align-items: center;
+    justify-content: flex-end;
+    padding-right: 0.5rem;
+`;
+
+export const NavbarInnerContainer = styled.div`
+    width: 100%;
+    height: 100%;
+    display: flex;
+`;
+
+export const NavbarLinkContainer = styled.div`
+    display: flex;
+    gap: 1rem;
+    /* flex-wrap: wrap; */
+`;
+
+export const NavbarLink = styled(Link)`
+    color: white;
+    font-size: 2rem;
+    text-decoration: none;
+    font-family: Arial, Helvetica, sans-serif;
+
+    &:hover{
+        transform: scale(1.02);
+    }
+`;
+
+export const LogoContainer = styled.img`
+    max-width: 180px;
+    height: 100%;
 `
+
+export const NavbarExtendedContainer = styled.div``;
+
+
